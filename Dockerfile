858a22bab717679abdabdd61fbd3c41f7fd217c6
DarkOmonS <window5slight@gmail.com>
2021-02-04 21:43:45 +0600

    added docker volume

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM golang
-
 RUN mkdir -p /usr/src/GoWebProject/
 WORKDIR /usr/src/GoWebProject/
-
 COPY . /usr/src/GoWebProject/
-
 CMD ["go", "run", "./cmd/web"]
--- pkg/db.sql
+++ pkg/db.sql
@@ -1,4 +1,4 @@
-create table snippets (
+create table if not exists snippets (
     id serial not null primary key,
     title varchar(100) not null,
     content text not null,
